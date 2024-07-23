cb1e6f4157979e5d7539f909b451899ac5ed10b2
ristukaJJang <ghwjd5585@gmail.com>
2024-07-23 16:50:51 +0900

    07-23 16:50

--- Constraints.sql
+++ Constraints.sql
@@ -45,7 +45,7 @@ update unique_table set unique_column = 1 where unique_column = 2;
 -- (insert, update)
 create table key_table (
 	primary_column int primary key,
-    surrogate_column ikey_tablent not null unique
+    surrogate_column int not null unique
 );
 
 -- 기본키 제약 조건 : not null + unique 모두 만족해야 함
@@ -73,5 +73,60 @@ create table foreign_table(
     references key_table(primary_column)
 );
 
+-- 외래키 제약조건이 적용된 컬럼에는 참조하고 있는 테이블의 컬럼에 값이 존재하지 않으면 삽입, 수정이 불가능
 insert into foreign_table values (1, 0);
 insert into foreign_table values (1, 1);
+
+update foreign_table set foreign1 = 2 where primary1 = 1;
+
+-- 참조되어지고 있는 테이블의 레코드는 수정/삭제가 불가능
+update key_table set primary_column = 2 where primary_column = 1;
+delete from key_table where primary_column = 1;
+
+-- 외래키 제약조건으로 참조되어지고 있는 테이블의 구조 변경 작업 불가능
+drop table key_table;
+alter table key_table modify column primary_column varchar(10);
+
+-- on update / on delete 옵션
+-- on update : 참조하고 있는 테이블의 기본키가 변경될 때 동작
+-- on delete : 참조하고 있는 테이블의 기본키가 삭제될 때 동작
+
+-- cascade : 참조되고 있는 테이블의 데이터가 삭제 또는 수정된다면, 참조하고 있는 테이블에서도 삭제/수정이 함께 일어남
+-- set null : 참조하고 있는 테이블의 데이터가 삭제 또는 수정된다면, 참조하고 있는 테이블의 데이터는 널로 지정
+-- restrict : 참조되고 있는 테이블의 데이터의 삭제 또는 수정을 불가능하게 함
+create table optional_foreign_table(
+	primary_column int primary key,
+    foreign_column int,
+    foreign key (foreign_column) references key_table (primary_column)
+    on update cascade
+    on delete set null
+);
+
+insert into optional_foreign_table values(1, 1);
+select * from optional_foreign_table;
+
+drop table foreign_table;
+
+update key_table set primary_column = 2;
+
+delete from key_table;
+
+-- check 제약 조건 : 해당 컬럼에 값을 제한
+create table check_table(
+	primary_column int primary key,
+    check_column varchar(5) check(check_column in('남', '여'))
+);
+-- check 제약 조건이 걸린 컬럼에 조건에 해당하지 않은 값을 삽입, 수정할 수 없다.
+insert into check_table values (1, '남');
+insert into check_table values (2, '남자');
+update check_table set check_column = '여자';
+
+-- default 제약 조건 : 해당 컬럼에 삽입시 값이 지정되지 않으면 기본값으로 지정하는 제약
+create table default_table(
+-- auto_increment : 기본키가 정수형일 때, 기본키의 값을 1씩 증가하는 값으로 자동 지정
+	primary_column int primary key auto_increment,
+    column1 int,
+    default_column int default 10
+);
+
+insert into default_table (column1) values (99);
